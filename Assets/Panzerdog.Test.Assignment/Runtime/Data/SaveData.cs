8b1f501a4f3c43c2f7da40f0403a9434d52f20fb
Artyom Babkin <dr.v991149@gmail.com>
2023-09-03 01:08:48 +0300

    refactor: moved ScoreAndLevelData to a separate file

--- Assets/Panzerdog.Test.Assignment/Runtime/Data/SaveData.cs
+++ Assets/Panzerdog.Test.Assignment/Runtime/Data/SaveData.cs
@@ -19,22 +19,4 @@ namespace Panzerdog.Test.Assignment.Data
             return $"{nameof(Rating)}=[{Rating}], {nameof(Experience)}=[{Experience}]";
         }
     }
-
-    //TODO: вынести в отдельный файл
-    public struct ScoreAndLevelData
-    {
-        public int Score { get; set; }
-        public int Level { get; set; }
-
-        public ScoreAndLevelData(ScoreAndLevelData other)
-        {
-            Score = other.Score;
-            Level = other.Level;
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(Score)}={Score}, {nameof(Level)}={Level}";
-        }
-    }
 }
---
+++ Assets/Panzerdog.Test.Assignment/Runtime/Data/ScoreAndLevelData.cs
@@ -0,0 +1,19 @@
+﻿namespace Panzerdog.Test.Assignment.Data
+{
+    public struct ScoreAndLevelData
+    {
+        public int Score { get; set; }
+        public int Level { get; set; }
+
+        public ScoreAndLevelData(ScoreAndLevelData other)
+        {
+            Score = other.Score;
+            Level = other.Level;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Score)}={Score}, {nameof(Level)}={Level}";
+        }
+    }
+}
