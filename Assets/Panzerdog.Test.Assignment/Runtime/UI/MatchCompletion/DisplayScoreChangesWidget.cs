a3893695d8e705b94c255d715dabbdb2b527e9e4
Artyom Babkin <dr.v991149@gmail.com>
2023-09-03 22:31:16 +0300

    refactor: removed magic numbers feat: added some more clear cutoff between progress bar changes

--- Assets/Panzerdog.Test.Assignment/Runtime/UI/MatchCompletion/DisplayScoreChangesWidget.cs
+++ Assets/Panzerdog.Test.Assignment/Runtime/UI/MatchCompletion/DisplayScoreChangesWidget.cs
@@ -13,6 +13,7 @@ namespace Panzerdog.Test.Assignment.UI.MatchCompletion
     public class DisplayScoreChangesWidget : MonoBehaviour
     {
         [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private float _fadeDuration = 1f;
         [SerializeField] private Transform _changesContainer;
         [SerializeField] private ProgressBar _progressBar;
         [SerializeField] private TMP_Text _levelText;
@@ -41,7 +42,7 @@ namespace Panzerdog.Test.Assignment.UI.MatchCompletion
 
         public async Task Show()
         {
-            await _canvasGroup.DOFade(1, 1).Play(_cancellationTokenSource.Token).AsyncWaitForCompletion();
+            await _canvasGroup.DOFade(1, _fadeDuration).Play(_cancellationTokenSource.Token).AsyncWaitForCompletion();
             
             _scoreChanges.ObserveAdd().Subscribe(async x =>
             {
@@ -56,15 +57,22 @@ namespace Panzerdog.Test.Assignment.UI.MatchCompletion
         
         private async Task OnScoreChanged(DictionaryAddEvent<ScoreChangeData, List<ScoreChangeStepData>> scoreChange)
         {
+            const int betweenProgressBarUpdatesDelay = 300;
+            
             var changeView = Instantiate(displayScoreChangeWidgetPrefab, _changesContainer);
-            _ = changeView.Show(scoreChange.Key, 1, _cancellationTokenSource.Token);
+            _ = changeView.Show(scoreChange.Key, _fadeDuration, _cancellationTokenSource.Token);
 
             foreach (var state in scoreChange.Value)
             {
                 _progressBar.Setup(state.CurrentScore, state.MaxScore);
                 _levelText.SetText(state.Level.ToString());
                 await _progressBar.SetValueAsync(state.NewScore, _cancellationTokenSource.Token);
+                await Task.Delay(betweenProgressBarUpdatesDelay, _cancellationTokenSource.Token).ContinueWith(EmptyMethod);
             }
+        }
+
+        private static void EmptyMethod(Task task)
+        {
         }
     }
 }
